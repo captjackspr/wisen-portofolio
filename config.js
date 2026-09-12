d245858c45c036f0c5f3c1f603d4d87349b74676
captjacksparrow <cjswisenvg@gmail.com>
2026-09-12 22:41:46 +0700

    Add admin panel

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 "use strict";
 
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://pvmybspjhtfdtrbuhruk.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2bXlic3BqaHRmZHRyYnVocnVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNzMyNzgsImV4cCI6MjEwNDc0OTI3OH0.2LPl5KajaL_5PGl8tBEkkFPmiunj5KHhUlEhmNiKB4k"
+  supabaseUrl: "https://pvmybspjhtfdtrbuhruk.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2bXlic3BqaHRmZHRyYnVocnVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNzMyNzgsImV4cCI6MjEwNDc0OTI3OH0.2LPl5KajaL_5PGl8tBEkkFPmiunj5KHhUlEhmNiKB4k"
 };
